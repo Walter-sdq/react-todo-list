0dbebf484a391489490645be3abdde3f22d3b60b
Walter <akhigbeabdulwahab354@gmail.com>
2024-08-11 01:02:22 +0100

    fix key error

---
+++ src/components/pages/todoDetails.component.jsx
@@ -0,0 +1,26 @@
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "../hooks/fetchListsData.component";
+const Details = () => {
+    const { id } = useParams()
+    const { data: list, err, loading } = useFetch(`http://localhost:8000/lists/${id}`)
+    return (
+        <div className="todoDetail">
+            {loading && <div>Loading...</div>}
+            {err && <div>{err}</div>}
+            {list && (
+                <div className="details">
+                    <h2 className="detail-head">
+                        {list.name}
+                    </h2>
+                    <p className="detail-body">
+                        {list.body}
+                    </p>
+                </div>
+
+            )}
+
+        </div>
+    );
+}
+
+export default Details;
